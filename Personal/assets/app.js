144dfd64c8f9d039d12764fc892170c0893a7a9a
Juanchomoles <119575624+Juanchomoles@users.noreply.github.com>
2024-01-08 13:37:31 +0100

    Subiendo archivo personal

---
+++ Personal/assets/app.js
@@ -0,0 +1,15 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.css'
+
+// Bootstrap 5 imports
+import 'bootstrap/dist/css/bootsrap.min.css'
+import 'bootstrap'
+import '@popperjs/core'
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
